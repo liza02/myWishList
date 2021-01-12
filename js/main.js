9c7f7c2c0381fec8a55ffad76136ad2038c94817
liza02 <lisa57car@gmail.com>
2021-01-12 19:14:07 +0100

    affichage une liste

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
 })
+
+
+function copyClipboard() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    alert("URL copié dans le presse papier : " + copyText.value);
+}

d6cfe781602ebbda9435bf2efeb0f0a20894589e
liza02 <lisa57car@gmail.com>
2021-01-15 21:28:36 +0100

    badge item reservé/disponible dans liste

--- src/vue/VueListe.php
+++ src/vue/VueListe.php
@@ -312,22 +312,19 @@ class VueListe
                     $description = $items['descr'];
                 }
                 if ($items['reserve'] == "false"){
-                    $boutonmodification = <<<FIN
-<a type="submit" class="btn btn-warning" href="$url_modifier" role="button"><span class="fa fa-pencil"></span> Modifier</a>
-<p><span class="badge badge-success">DISPONIBLE</span></p>
-FIN;
+                    $boutonmodification = "<a type=\"submit\" class=\"btn btn-warning\" href=\"$url_modifier\" role=\"button\"><span class=\"fa fa-pencil\"></span> Modifier</a>";
+                    $isReserved = "<h7><span class=\"nom_item\">{$items['nom']} </span><span class=\"badge badge-success\">DISPONIBLE</span></h7>";
                 }else {
-                    $boutonmodification = <<<FIN
-<a class="btn btn-secondary disabled" href="$url_modifier" role="button" aria-disabled="true"><span class="fa fa-pencil" ></span> Modifier</a>
-<p><span class="badge badge-secondary">RESERVÉ</span></p>
-FIN;
+                    $boutonmodification = "<a class=\"btn btn-secondary disabled\" href=\"$url_modifier\" role=\"button\" aria-disabled=\"true\"><span class=\"fa fa-pencil\" ></span> Modifier</a>";
+                    $isReserved = "<h7><span class=\"nom_item\">{$items['nom']} </span><span class=\"badge badge-secondary\">RESERVÉ</span></h7>";
+
                 }
                 $html_items .= <<<FIN
                 <div class="col-3 Itembox">
                     <div class="card h-100 mb-3 border-secondary">
                       <img class="card-img-top image_item" src="$image" onError="this.onerror=null;this.src='../img/default.png';">
                       <div class="card-body">
-                        <h5 class="card-title">{$items['nom']}</h5>
+                        <h7 class="card-title"> {$isReserved} </h7>
                         <p class="card-text">{$description}</p>
                         </div>
                       <footer class="bouton_footer text-center">
